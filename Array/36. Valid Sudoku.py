dc82b705e55100844b12ce03d65d83f08e194404
Priyansh Singh <priyanshsingh855@gmail.com>
2026-01-29 21:49:24 +0530

    Update 36. Valid Sudoku.py

--- Array/36. Valid Sudoku.py
+++ Array/36. Valid Sudoku.py
@@ -19,6 +19,7 @@ class Solution(object):
                     return False
                 cols[c].add(val)
 
+                
                 box=(r//3,c//3)
                 if box not in boxes:
                     boxes[box]=set()
@@ -28,3 +29,21 @@ class Solution(object):
                 boxes[box].add(val)
 
         return True
+
+# -------- 3x3 BOX LOGIC (IMPORTANT) --------
+# Each 3x3 box is identified using (row // 3, col // 3)
+# All cells that give the same (row//3, col//3) belong to the SAME box
+#
+# boxes is a dictionary:
+#   key   -> (box_row, box_col)
+#   value -> set of numbers already seen in that box
+#
+# Steps:
+# 1. Find which box the current cell belongs to
+# 2. If this box is visited for the first time, create an empty set for it
+# 3. If the value already exists in the box set -> Sudoku is INVALID
+# 4. Otherwise, add the value to the box set
+#
+# Example:
+# Cell (4,7) -> (4//3, 7//3) -> (1,2) -> middle-right box
+# ------------------------------------------
