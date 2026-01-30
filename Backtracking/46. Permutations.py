7d891a050227588e687de840da02e8b8131be537
Priyansh Singh <priyanshsingh855@gmail.com>
2026-01-30 18:25:05 +0530

    Create 46. Permutations.py

---
+++ Backtracking/46. Permutations.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        result = []
+
+        if len(nums)==1:
+            return [nums[:]]  
+
+        for i in range(len(nums)):
+            n=nums.pop(0)    
+
+            perms=self.permute(nums)  
+
+            for perm in perms:
+                perm.append(n)          
+                result.append(perm)
+
+            nums.append(n)   
+
+        return result
