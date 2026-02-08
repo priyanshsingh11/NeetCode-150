a5824f7c7d99cb4aa5498d440003a834ac4bcd4f
Priyansh Singh <priyanshsingh855@gmail.com>
2026-02-08 17:02:09 +0530

    Create 91. Decode Ways.py

---
+++ DP/91. Decode Ways.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numDecodings(self, s):
+        dp={}
+
+        def dfs(i):
+            if i==len(s):
+                return 1
+
+            if s[i]=="0":
+                return 0
+
+            if i in dp:
+                return dp[i]
+
+            res=dfs(i+1)
+
+            if (i+1<len(s) and
+                (s[i]=="1" or (s[i]=="2" and s[i+1] in "0123456"))):
+                res+=dfs(i+2)
+
+            dp[i]=res
+            return res
+
+        return dfs(0)
