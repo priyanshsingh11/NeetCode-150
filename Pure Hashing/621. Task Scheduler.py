4c708b042e7177cdd62d10212cda70a77c40ab52
Priyansh Singh <priyanshsingh855@gmail.com>
2026-02-13 23:23:18 +0530

    Create 621. Task Scheduler.py

---
+++ Pure Hashing/621. Task Scheduler.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        freq = {}
+
+        # count frequency properly
+        for task in tasks:
+            freq[task] = freq.get(task, 0) + 1
+
+        maxFreq = max(freq.values())
+
+        maxi = 0
+        for value in freq.values():
+            if value == maxFreq:
+                maxi += 1
+
+        result = (maxFreq - 1) * (n + 1) + maxi
+
+        return max(len(tasks), result)
