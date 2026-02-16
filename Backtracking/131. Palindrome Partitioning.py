bd1e14730db79a4ec8a46adc0c73b77b89f1ecf7
Priyansh Singh <priyanshsingh855@gmail.com>
2026-02-16 17:27:40 +0530

    Create 131. Palindrome Partitioning.py

---
+++ Backtracking/131. Palindrome Partitioning.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def partition(self, s):
+        result=[]
+
+        def isPallindrom(sub):
+            return sub==sub[::-1]  
+
+        def backtrack(start,path):
+            if start==len(s):
+                result.append(path[:])
+                return 
+            
+            for end in range(start+1,len(s)+1):
+                substring=s[start:end]
+
+                if isPallindrom(substring):
+                    path.append(substring)
+                    backtrack(end,path)
+                    path.pop()
+        
+        backtrack(0,[])
+        return result 
+        
