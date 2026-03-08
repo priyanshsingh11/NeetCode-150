0971f64c6111e7f35f85c6ec2b62c61fe52e198c
Priyansh Singh <priyanshsingh855@gmail.com>
2026-03-08 19:18:35 +0530

    Create 230. Kth Smallest Element in a BST.py

---
+++ Trees & Binary Trees/230. Kth Smallest Element in a BST.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def kthSmallest(self, root, k):
+        ans=[]
+        def inorder(node):
+            if not node:
+                return 
+            
+            inorder(node.left)
+            ans.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        return ans[k-1]
