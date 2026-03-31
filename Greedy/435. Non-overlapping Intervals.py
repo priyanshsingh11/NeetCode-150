f6f4c076597982ac79af3b4862a1650e8c03677c
Priyansh Singh <priyanshsingh855@gmail.com>
2026-04-01 00:24:42 +0530

    Create 435. Non-overlapping Intervals.py

---
+++ Greedy/435. Non-overlapping Intervals.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        intervals.sort(key = lambda x:x[1])
+        count=0
+
+        prev_end=intervals[0][1]
+        
+        for i in range(1,len(intervals)):
+            start,end=intervals[i]
+
+            if start<prev_end: count+=1 
+
+            else:
+                prev_end=end
+        
+        return count
